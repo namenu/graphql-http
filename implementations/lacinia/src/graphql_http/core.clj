87a5907583d4d945045618d8600c152498bf53ef
Hyunwoo Nam <namenu@gmail.com>
2024-06-16 22:16:29 +0900

    Add lacinia implementation

--- implementations/lacinia/src/graphql_http/core.clj
+++ implementations/lacinia/src/graphql_http/core.clj
@@ -2,6 +2,8 @@
   (:require [clojure.data.json :as json]
             [clojure.edn :as edn]
             [ring.adapter.jetty :as jetty]
+            [reitit.ring.middleware.parameters :refer [parameters-middleware]]
+            [reitit.ring :as ring]
             [com.walmartlabs.lacinia :refer [execute]]
             [com.walmartlabs.lacinia.schema :as schema]
             [com.walmartlabs.lacinia.util :refer [inject-resolvers]]))
@@ -10,30 +12,45 @@
   [context arguments value]
   (let [{:keys [episode]} arguments]
     (if (= episode :NEWHOPE)
-      {:id 1000
-       :name "Luke"
+      {:id         1000
+       :name       "Luke"
        :homePlanet "Tatooine"
-       :appearsIn ["NEWHOPE" "EMPIRE" "JEDI"]}
-      {:id 2000
-       :name "Lando Calrissian"
+       :appearsIn  ["NEWHOPE" "EMPIRE" "JEDI"]}
+      {:id         2000
+       :name       "Lando Calrissian"
        :homePlanet "Socorro"
-       :appearsIn ["EMPIRE" "JEDI"]})))
+       :appearsIn  ["EMPIRE" "JEDI"]})))
 
 (def star-wars-schema
   (-> "schema.edn"
       slurp
       edn/read-string
-      (inject-resolvers {:Query/hero get-hero
+      (inject-resolvers {:Query/hero  get-hero
                          :Query/droid (constantly {})})
       schema/compile))
 
 (defn handler [request]
-  {:status 200
+  {:status  200
    :headers {"Content-Type" "application/json"}
-   :body (let [query (get-in request [:query-params :query])
-               result (execute star-wars-schema query nil nil)]
-           (json/write-str result))})
+   :body    (let [query  (get-in request [:query-params :query])
+                  _      (clojure.pprint/pprint (:query-params request))
+                  result (execute star-wars-schema query nil nil)]
+              (json/write-str result))})
+
+(def router
+  (ring/router
+    ["/graphql" {:get  {:handler    #'handler
+                        :middleware [parameters-middleware]}
+                 :post {:handler #'handler}}]))
+
+(def app
+  (ring/ring-handler router))
+
+(defn -main
+  "Starts the Jetty server."
+  [& args]
+  (jetty/run-jetty #'app {:port  3000
+                          :join? false}))
 
 (comment
-  (jetty/run-jetty handler {:port  3000
-                            :join? false}))
+  (-main))
